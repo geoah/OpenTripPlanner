6f5186362dce21befa275a864c3f781eaf005f3c
George Antoniadis <george@noodles.gr>
2015-08-22 21:20:25 +0300

    Switch to metric

--- src/client/js/otp/config.js
+++ src/client/js/otp/config.js
@@ -121,7 +121,7 @@ otp.config = {
     showLogo            : true,
     showTitle           : true,
     showModuleSelector  : true,
-    metric              : false,
+    metric              : true,
 
 
     /**
